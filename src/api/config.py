575ddae2ed4daaf3a245d5081cf010a20d327999
D0nG4667 <D0nG4667@users.noreply.github.com>
2024-07-09 07:24:58 +0300

    Add api with redis caching feature and complete type hinting

---
+++ src/api/config.py
@@ -0,0 +1,15 @@
+import os
+
+ONE_DAY_SEC = 24*60*60
+
+ONE_WEEK_SEC = ONE_DAY_SEC*7
+
+XGBOOST_URL = "https://raw.githubusercontent.com/valiantezabuku/Building-Machine-Learning-API-s-with-FastAPI-Collabo/develop/dev/models/xgboost.joblib"
+
+RANDOM_FOREST_URL = "https://raw.githubusercontent.com/valiantezabuku/Building-Machine-Learning-API-s-with-FastAPI-Collabo/develop/dev/models/random_forest.joblib"
+
+ENCODER_URL = "https://raw.githubusercontent.com/valiantezabuku/Building-Machine-Learning-API-s-with-FastAPI-Collabo/develop/dev/models/encoder.joblib"
+
+BASE_DIR = './'  # Where Unicorn server runs from
+
+ENV_PATH = os.path.join(BASE_DIR, 'src/api/.env')
